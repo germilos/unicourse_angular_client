68392192ff774f759769bb45ab423f7431bdc564
Runge37 <ger.milos.bt@gmail.com>
2019-02-18 19:30:28 +0100

    Swaps working

--- src/app/course-edit/course-edit.component.ts
+++ src/app/course-edit/course-edit.component.ts
@@ -17,8 +17,8 @@ export class CourseEditComponent implements OnInit {
   course: any = {};
   studyPrograms: Array<any>;
   departments: Array<any>;
-  selectLecturers: Array<any>;
-  lecturers: Array<any>;
+  selectLecturers: Array<any> = [];
+  lecturers: Array<any> = [];
   courseForm: FormGroup;
   courseUnits: FormArray;
   sub: Subscription;
@@ -44,6 +44,8 @@ export class CourseEditComponent implements OnInit {
       lecturers: [],
       courseUnits: this.formBuilder.array([this.createItem()])
     });
+    this.courseForm.controls.lecturers.value = [];
+    this.courseForm.controls.selectLecturers.value = [];
     this.studyProgramService.getAll().subscribe((studyPrograms) => {
       if (studyPrograms) {
         this.studyPrograms = studyPrograms;
@@ -62,7 +64,9 @@ export class CourseEditComponent implements OnInit {
     });
     this.lecturerService.getAll().subscribe((lecturers) => {
       if (lecturers) {
+        console.log("Lecturers from service: ", lecturers);
         this.selectLecturers = lecturers;
+        console.log("Lecturers in service: ", this['selectLecturers']);
       } else {
         console.log(`There was an error loading lecturers, returning to course list`);
         this.gotoList();
@@ -85,30 +89,11 @@ export class CourseEditComponent implements OnInit {
     });
   }
 
-  // swapLecturers(from: string, to: string) {
-    
-  //   // TODO: Refactor for immutability
-  //   let lecturersForSwap = this.courseForm.controls.selectLecturers;
-  //   if (this.courseForm.controls.lecturers.value) {
-  //     for (let i = 0; i < lecturersForSwap.value.length; i++) {
-  //       this.courseForm.controls.lecturers.value.push(lecturersForSwap.value[i]);
-  //       this.lecturers.push(lecturersForSwap.value[i]);
-  //       this.courseForm.controls.selectLecturers.value.splice(this.courseForm.controls.selectLecturers.value.indexOf(lecturersForSwap.value[i]), 1);
-  //       this.selectLecturers.splice(this.selectLecturers.indexOf(lecturersForSwap.value[i]), 1);
-  //     }
-  //   } else {
-  //     this.courseForm.controls.lecturers = lecturersForSwap;
-  //     this.lecturers = this.courseForm.controls.lecturers.value;
-  //     for (let i = 0; i < this.lecturers.length; i++) {
-  //       this.selectLecturers.splice(this.selectLecturers.indexOf(this.lecturers[i]), 1);
-  //     }
-  //   }
-  // }
-
   swapLecturers(from: string, to: string) {
     
     // TODO: Refactor for immutability
     let lecturersForSwap = this.courseForm.controls[from];
+    this.courseForm.controls[to] = lecturersForSwap;
     if (this.courseForm.controls[to].value) {
       for (let i = 0; i < lecturersForSwap.value.length; i++) {
         this.courseForm.controls[to].value.push(lecturersForSwap.value[i]);
@@ -116,13 +101,33 @@ export class CourseEditComponent implements OnInit {
         this.courseForm.controls[from].value.splice(this.courseForm.controls[from].value.indexOf(lecturersForSwap.value[i]), 1);
         this[from].splice(this[from].indexOf(lecturersForSwap.value[i]), 1);
       }
-    } else {
-      this.courseForm.controls[to] = lecturersForSwap;
-      this[to] = this.courseForm.controls[to].value;
-      for (let i = 0; i < this[to].length; i++) {
-        this[from].splice(this[from].indexOf(this[to][i]), 1);
-      }
+    } 
+    // else {
+    //   // this.courseForm.controls[to] = lecturersForSwap;
+    //   this[to] = this.courseForm.controls[to].value;
+    //   for (let i = 0; i < this[to].length; i++) {
+    //     this[from].splice(this[from].indexOf(this[to][i]), 1);
+    //   }
+    // }
+  }
+
+  swapLecturersAll(from: string, to: string) {
+    console.log("In: " + from, this[from]);
+    console.log("In: " + to, this[to]);
+    let lecturersForSwap = this[from].map(result => {
+      return result;
+    });
+    console.log("For swap: ", lecturersForSwap);
+    if (!this.courseForm.controls[to].value) {
+      this.courseForm.controls[to].value = [];
     }
+    console.log("Swaps: ", lecturersForSwap);
+    for(let i = 0; i < lecturersForSwap.length; i++) {
+      this[to].push(lecturersForSwap[i]);
+      this.courseForm.controls[to].value.push(lecturersForSwap[i]);
+      this[from].splice(this[from].indexOf(lecturersForSwap[i]), 1);
+    }
+    
   }
 
   gotoList() {
