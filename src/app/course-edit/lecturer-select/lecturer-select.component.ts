5b8aa9a25702d30b9c557e2ee52e20d554111b3e
Milos <ger.milos.bt@gmail.com>
2019-04-29 02:15:47 +0200

    Finished refactoring lecturer-select.component

--- src/app/course-edit/lecturer-select/lecturer-select.component.ts
+++ src/app/course-edit/lecturer-select/lecturer-select.component.ts
@@ -12,12 +12,10 @@ import { SelectLecturersDirective } from 'src/app/select-lecturers.directive';
 export class LecturerSelectComponent implements OnInit {
 
   @Output() private formReady: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
-  @Input() course: any;
+  @Input() private course: any;
   private formGroup: FormGroup;
   private selectLecturers: Lecturer[];
   private lecturers: Lecturer[];
-  @ViewChild('selLects') selLects;
-  @ViewChild('selectedLecturers') selectdLects;
   private flag: boolean = false;
 
   constructor(private fb: FormBuilder,
@@ -31,76 +29,72 @@ export class LecturerSelectComponent implements OnInit {
   ngOnInit() {
     this.selectLecturers = [];
     this.lecturers = [];
-    // this.formReady.emit(this.formGroup);
     this.lecturerService.getAll()
       .subscribe((res: Lecturer[]) => {
         this.selectLecturers = res;
       });
   }
 
+  // Executed only once course is loaded
   ngOnChanges() {
-    console.log("final group: ", this.formGroup.controls.lecturers.value);
-    if (this.course && this.course.lecturers.length != 0 && !this.flag) {
+    if (this.course.lecturers && !this.flag) {
       this.flag = true;
-      console.log("Course lecturers: ", this.course.lecturers);
-      let controlLects = <FormControl>this.formGroup.controls.lecturers;
-      console.log("Aha lecturers: ", this.formGroup.controls.lecturers.value);
-      for (let i = 0; i < this.course.lecturers.length; i++) {
-        controlLects.value.push(this.course.lecturers[i]);
-        this.lecturers.push(this.course.lecturers[i]);
+      this.course.lecturers.forEach((lecturer: Lecturer) => {
+        this.formGroup.controls['lecturers'].value
+          .push(lecturer);
+        this.lecturers.push(lecturer);
         this.selectLecturers.splice(this.selectLecturers.map(function (e) {
           return e.id;
-        }).indexOf(this.course.lecturers[i].id), 1);
-      }
-      
-      console.log("Pushed: ", controlLects.value);
+        }).indexOf(lecturer.id), 1);
+      });
     }
   }
 
-  // TODO: Refactor for immutability
+  // Swap only selected lecturers
   swapLecturers(from: string, to: string) {
+
+    // Get the highlighted (selected) lecturers
     let lecturersForSwap = this.formGroup.controls[from].value.map(result => {
       return result;
     });
 
-    for (let i = 0; i < lecturersForSwap.length; i++) {
-      // this.formGroup.controls[to].value.push(lecturersForSwap[i]);
-      this[from].splice(this[from].indexOf(lecturersForSwap[i]), 1);
-      this[to].push(lecturersForSwap[i]);
-      // this.formGroup.controls[from].value.splice(
-      //   this.formGroup.controls[from].value.indexOf(lecturersForSwap[i]), 1);
+    // Moves from origin to destination select list
+    lecturersForSwap.forEach((lecturer: Lecturer) => {
+      this[from].splice(this[from].indexOf(lecturer), 1); this.formGroup.controls[to].value.push(lecturer);
+      this.formGroup.controls[from].value.splice(
+        this.formGroup.controls[from].value.indexOf(lecturer), 1);
+      this[to].push(lecturer);
+    });
 
-    }
-    
+    // Unselect lecturers in 'selectLecturers'
     this.formGroup.controls['selectLecturers'].setValue([]);
-    this.formGroup.controls['lecturers'].setValue([]);
-    console.log("Lecturers before adding: ", this.formGroup.controls['lecturers'].value);
-    for(let i = 0; i < this.lecturers.length; i++) {
-      this.formGroup.controls['lecturers'].value.push(this.lecturers[i]);
-    }
-    console.log("Lecturers after adding: ", this.formGroup.controls['lecturers'].value);
+
+    // Mark all Lecturers in 'lecturers' element as selected
+    this.lecturers.forEach((lecturer: Lecturer) => {
+      this.formGroup.controls['lecturers'].value.push(lecturer);
+    });
     this.formReady.emit(this.formGroup);
   }
 
   swapLecturersAll(from: string, to: string): void {
-    // Get all lecturers from source list
-    console.log(from + " ", this.formGroup.controls[from].value);
-    console.log(to + " ", this.formGroup.controls[to].value);
+
+    // Get all lecturers from origin select list
     let lecturersForSwap = this[from].map(result => {
       return result;
     });
 
     /*
-     * Iterate thorugh source and push to target list (control and HTML element)
-     * Splice from source
+     * Iterate thorugh origin and push to destination list (control and HTML)
+     * element
+     * Splice from origin
      */
-    for (let i = 0; i < lecturersForSwap.length; i++) {
-      this[to].push(lecturersForSwap[i]);
-      this.formGroup.controls[to].value.push(lecturersForSwap[i]);
+    lecturersForSwap.forEach((lecturer: Lecturer) => {
+      this[to].push(lecturer);
+      this.formGroup.controls[to].value.push(lecturer);
       this.formGroup.controls[from].value.splice(
-        this.formGroup.controls[from].value.indexOf(lecturersForSwap[i]), 1);
-      this[from].splice(this[from].indexOf(lecturersForSwap[i]), 1);
-    }
+        this.formGroup.controls[from].value.indexOf(lecturer), 1);
+      this[from].splice(this[from].indexOf(lecturer), 1);
+    });
 
     this.formReady.emit(this.formGroup);
   }
