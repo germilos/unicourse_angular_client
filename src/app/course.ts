34dac850fad7466b9919d139fc78a7132175fb4f
Milos <ger.milos.bt@gmail.com>
2019-04-29 16:15:45 +0200

    Course class left

---
+++ src/app/course.ts
@@ -0,0 +1,9 @@
+import { Department } from './department';
+
+export class Course {
+
+    // constructor(private _name: string, private _goal: string,
+    //             private _status: string, private espb: number,
+    //             private _department: Department, 
+    //             private _studyProgram: StudyProgram)
+}
